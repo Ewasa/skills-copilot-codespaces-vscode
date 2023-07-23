746bb8d1172be613bf6b908ceead704a6b8483e8
Ewa <70981571+Ewasa@users.noreply.github.com>
2023-07-23 12:19:10 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,87 @@
+// Create web server
+
+var express = require('express');
+var router = express.Router();
+
+var Comment = require('../models/comment');
+var Post = require('../models/post');
+
+// GET /comments
+router.get('/', function(req, res, next) {
+  Comment.find(function(err, comments) {
+    if (err) {
+      return next(err);
+    }
+    res.json(comments);
+  });
+});
+
+// POST /comments
+router.post('/', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.save(function(err, comment) {
+    if (err) {
+      return next(err);
+    }
+    res.status(201).json(comment);
+  });
+});
+
+// GET /comments/:id
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  Comment.findOne({_id: id}, function(err, comment) {
+    if (err) {
+      return next(err);
+    }
+    if (!comment) {
+      return res.status(404).json({message: 'Comment not found'});
+    }
+    res.json(comment);
+  });
+});
+
+// PUT /comments/:id
+router.put('/:id', function(req, res, next) {
+  var id = req.params.id;
+  Comment.findOne({_id: id}, function(err, comment) {
+    if (err) {
+      return next(err);
+    }
+    if (!comment) {
+      return res.status(404).json({message: 'Comment not found'});
+    }
+    comment.content = req.body.content;
+    comment.save(function(err, comment) {
+      if (err) {
+        return next(err);
+      }
+      res.json(comment);
+    });
+  });
+});
+
+// DELETE /comments/:id
+router.delete('/:id', function(req, res, next) {
+  var id = req.params.id;
+  Comment.findOne({_id: id}, function(err, comment) {
+    if (err) {
+      return next(err);
+    }
+    if (!comment) {
+      return res.status(404).json({message: 'Comment not found'});
+    }
+    comment.remove(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.json(comment);
+    });
+  });
+});
+
+// GET /comments/:id/post
+router.get('/:id/post', function(req, res, next) {
+  var id = req.params.id;
+  Comment.findOne({_id: id}, function(err, comment) {
+    if (err)
